c9ff846a11110529ec44288482db8e7c1113bd4d
Alex Dobson <alex.dobson@live.com>
2014-12-09 13:21:27 -0700

    Added archive template and loop.

---
+++ dobsondev-theme/archive.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying all posts.
+ *
+ * @package     DobsonDev Theme
+ * @author      Alex Dobson - http://dobsondev.com/
+ */
+
+get_header();
+do_action('dobsondev_before_content');
+get_template_part( 'loop', 'archive' );
+do_action('dobsondev_menu');
+do_action('dobsondev_after_content');
+get_footer();
+?>
